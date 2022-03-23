a7436f9523f9ea110109a7d24a3a14bfeb32702e
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-03-23 13:29:33 -0400

    Delete HTMLCompiler directory

--- HTMLCompiler/uploader.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Upload from shell script"
-git branch -M main
-git remote add origin https://github.com/jasonglenevans/GvbvdxxMod2HTML.git
-git push -f origin main
